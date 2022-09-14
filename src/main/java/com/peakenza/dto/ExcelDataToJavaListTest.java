2f9892e33c6eb164c357f9e3a7ae549150e84b4e
Nirav <nrvmodi@gmail.com>
2022-09-14 16:32:27 +0530

    changes

--- src/main/java/com/peakenza/dto/ExcelDataToJavaListTest.java
+++ src/main/java/com/peakenza/dto/ExcelDataToJavaListTest.java
@@ -1,16 +1,19 @@
 package com.peakenza.dto;
 
 import com.poiji.bind.Poiji;
+import com.poiji.exception.PoijiExcelType;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.List;
 
 public class ExcelDataToJavaListTest {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
 
         File file = new File("c:/test.xlsx");
-        List<EmployeeExcel> invoices = Poiji.fromExcel(file, EmployeeExcel.class);
+        List<EmployeeExcel> invoices = Poiji.fromExcel(new FileInputStream(file), PoijiExcelType.XLSX, EmployeeExcel.class);
         System.out.println("Printing List Data: " + invoices);
     }
 }
--- src/main/java/com/peakenza/service/FileProcessorService.java
+++ src/main/java/com/peakenza/service/FileProcessorService.java
@@ -1,11 +1,15 @@
 package com.peakenza.service;
 
 import com.amazonaws.services.transfer.model.S3FileLocation;
+import com.peakenza.dto.EmployeeExcel;
+import com.poiji.bind.Poiji;
+import com.poiji.exception.PoijiExcelType;
 import org.apache.commons.beanutils.PropertyUtils;
 import software.amazon.awssdk.core.ResponseInputStream;
 
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
+import java.util.List;
 import java.util.Map;
 
 public class FileProcessorService {
@@ -17,7 +21,7 @@ public class FileProcessorService {
         AwsS3Service awsS3Service = new AwsS3Service();
 
         ResponseInputStream inputStream = awsS3Service.readFile(s3FileLocation.getBucket(), s3FileLocation.getKey());
-
+        List<EmployeeExcel> employeeExcels = Poiji.fromExcel(inputStream, PoijiExcelType.XLS, EmployeeExcel.class);
         //TODO: If you get file from s3 then you can pass that file in below method and validate it.
         validateFile(event);
 
